39a684c8b25b884a945408663cf2c22b638b0740
fernandonakagawa <fernando.nakagawa@sesisenaipr.org.br>
2021-03-11 22:15:00 -0300

    Commit inicial

---
+++ Aula05_WF_01/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Aula05_WF_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbSenha = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.rbDesabilitado = new System.Windows.Forms.RadioButton();
+            this.rbHabilitado = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Papyrus", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(30, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(220, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Clique Aqui!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Castellar", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(254, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Olá Mundo!";
+            // 
+            // tbSenha
+            // 
+            this.tbSenha.Enabled = false;
+            this.tbSenha.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbSenha.Location = new System.Drawing.Point(391, 266);
+            this.tbSenha.MaxLength = 10;
+            this.tbSenha.Name = "tbSenha";
+            this.tbSenha.PasswordChar = '*';
+            this.tbSenha.Size = new System.Drawing.Size(121, 39);
+            this.tbSenha.TabIndex = 2;
+            this.tbSenha.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tbSenha.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(410, 311);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "OK";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(596, 88);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.checkBox1.Size = new System.Drawing.Size(131, 29);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Ligar a luz";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // rbDesabilitado
+            // 
+            this.rbDesabilitado.AutoSize = true;
+            this.rbDesabilitado.Location = new System.Drawing.Point(16, 19);
+            this.rbDesabilitado.Name = "rbDesabilitado";
+            this.rbDesabilitado.Size = new System.Drawing.Size(83, 17);
+            this.rbDesabilitado.TabIndex = 5;
+            this.rbDesabilitado.TabStop = true;
+            this.rbDesabilitado.Text = "Desabilitado";
+            this.rbDesabilitado.UseVisualStyleBackColor = true;
+            this.rbDesabilitado.CheckedChanged += new System.EventHandler(this.rbDesabilitado_CheckedChanged);
+            // 
+            // rbHabilitado
+            // 
+            this.rbHabilitado.AutoSize = true;
+            this.rbHabilitado.Location = new System.Drawing.Point(16, 42);
+            this.rbHabilitado.Name = "rbHabilitado";
+            this.rbHabilitado.Size = new System.Drawing.Size(72, 17);
+            this.rbHabilitado.TabIndex = 6;
+            this.rbHabilitado.TabStop = true;
+            this.rbHabilitado.Text = "Habilitado";
+            this.rbHabilitado.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbDesabilitado);
+            this.groupBox1.Controls.Add(this.rbHabilitado);
+            this.groupBox1.Location = new System.Drawing.Point(381, 372);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(131, 78);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Gaveta";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Aula05_WF_01.Properties.Resources.guia_estudos;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tbSenha);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Meu Primeiro Windows Forms";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbSenha;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RadioButton rbDesabilitado;
+        private System.Windows.Forms.RadioButton rbHabilitado;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Aula05_WF_01/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula05_WF_01
+{
+    public partial class Form1 : Form
+    {
+        private bool travar;
+        public Form1()
+        {
+            InitializeComponent();
+            this.travar = false;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(label1.Text.Equals("Bom dia!"))
+            {
+                label1.Text = "Olá Mundo!";
+            }
+            else
+            {
+                label1.Text = "Bom dia!";
+            }
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            if (!this.travar)
+            {
+                label1.Text = "HAHA!";
+                if (button1.Location.X < 560)
+                {
+                    button1.Location = new Point(560, 210);
+                }
+                else
+                {
+                    button1.Location = new Point(30, 210);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (tbSenha.Text.Equals("Senai"))
+            {
+                this.travar = true;
+                MessageBox.Show("Parabéns! Você conseguiu travar o botão!");
+                label1.Text = "Parabéns";
+            }
+            else
+            {
+                this.travar = false;
+                MessageBox.Show("SENHA INCORRETA!");
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            tbSenha.Visible = checkBox1.Checked;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rbDesabilitado_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbDesabilitado.Checked) tbSenha.Enabled = false;
+            else tbSenha.Enabled = true;
+        }
+    }
+}
